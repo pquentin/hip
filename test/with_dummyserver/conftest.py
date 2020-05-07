5a202ca04d18ae0d8ffad2ce0c1e25d4628c3572
Quentin Pradet <quentin.pradet@gmail.com>
2020-05-07 21:33:46 +0400

    Stop requiring anyio_backend arg in test functions

--- test/with_dummyserver/conftest.py
+++ test/with_dummyserver/conftest.py
@@ -16,15 +16,18 @@ except ImportError:
 
 ServerConfig = collections.namedtuple("ServerConfig", ["host", "port", "ca_certs"])
 
-test_all_backends = pytest.mark.parametrize(
-    "backend, anyio_backend",
-    [
-        pytest.param("trio", "trio", id="trio-native"),
-        pytest.param("anyio", "trio", id="anyio-trio"),
-        pytest.param("anyio", "curio", id="anyio-curio"),
-        pytest.param("anyio", "asyncio", id="anyio-asyncio"),
-    ],
-)
+
+def test_all_backends(func):
+    func = pytest.mark.usefixtures("anyio_backend")(func)
+    return pytest.mark.parametrize(
+        "backend, anyio_backend",
+        [
+            pytest.param("trio", "trio", id="trio-native"),
+            pytest.param("anyio", "trio", id="anyio-trio"),
+            pytest.param("anyio", "curio", id="anyio-curio"),
+            pytest.param("anyio", "asyncio", id="anyio-asyncio"),
+        ],
+    )(func)
 
 
 test_sync_backend = pytest.mark.parametrize("backend, anyio_backend", [("sync", None)])
